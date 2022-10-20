7102f3065ef6da9245c84b9491a72b5aa5bfef03
PanConPoolo2003 <10071533@cientifica.edu.pe>
2022-10-19 19:57:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/evaluacion/ventana3.java
@@ -0,0 +1,14 @@
+package com.example.evaluacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ventana3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana3);
+    }
+}
